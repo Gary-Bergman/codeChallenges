a4e8bf0684c4b503f2be71a73df5c0474bda41cf
Gary Bergman <garybergman00@gmail.com>
2020-10-15 17:55:05 -0400

    Solved maxNum.js

--- maxNum.js
+++ maxNum.js
@@ -9,3 +9,20 @@
 
 // Input: [ 3, 3, 3 ]
 // Output: 3
+
+
+
+const findMax = numArr => {
+    let maxNum = numArr[0];
+     for (let i = 1; i < numArr.length; i++) {
+       if (numArr[i] > maxNum) {
+         maxNum = numArr[i];
+       }
+     }
+     return maxNum;
+}
+
+console.log(findMax([1, 2, 3]));
+console.log(findMax([3, 6, 4, 5, 2, 1]));
+console.log(findMax([ 3, 3, 3 ]));
+console.log(findMax([5, 190, 3]));
