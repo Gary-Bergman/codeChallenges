25bb514e5e55d3010d5121aeaabb867f93860920
Gary Bergman <garybergman00@gmail.com>
2020-10-16 12:32:32 -0400

    Added an alternative solution to numVowels.js file

--- numVowels.js
+++ numVowels.js
@@ -24,3 +24,22 @@ console.log(numVowels("hello"));
 console.log(numVowels("you are great!"));
 console.log(numVowels("why?"));
 
+// Solution 2: ------------------------------
+
+const numVowels2 = str => {
+    let count = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    str = str.split("");
+    // Could add .toLowerCase() if str has any capital letters too
+
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++
+        }
+    }
+    return count;
+}
+
+console.log(numVowels2("hello"));
+console.log(numVowels2("you are great!"));
+console.log(numVowels2("why?"));
