10bfd83737982076077160d5ece370168a74b860
Gary Bergman <garybergman00@gmail.com>
2020-10-21 16:45:18 -0400

    Solved firstnorepeat

--- firstNoRepeat.js
+++ firstNoRepeat.js
@@ -11,5 +11,26 @@
 // Input: “what a wonderful day it has been!”
 // Output: "o"
 
+const noRepeat = str => {
+    let strArray = str.split("");
+    // console.log(strArray)
+    for (let i = 0; i < strArray.length; i++) {
+        let count = 0;
+        for (let j = 0; j < strArray.length; j++) {
+            if (strArray[i] === strArray[j]) {
+               count++; 
+            }
+        }
+        if (count === 1){
+            return strArray[i];
+        }
+    }
+}
 
+console.log(noRepeat("the quick brown fox jumps over the calm kitten quietly"));
 
+console.log(noRepeat("this hat is the greatest!"));
+
+console.log(noRepeat("what a wonderful day it has been!"));
+
+console.log(noRepeat("aabaaacbcdef"));
