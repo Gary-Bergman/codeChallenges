fb6c4c600e71746901d2e840fb13659ec7d3bf35
Gary Bergman <garybergman00@gmail.com>
2020-10-22 12:20:25 -0400

    Solved averageNum

--- averageNum.js
+++ averageNum.js
@@ -8,3 +8,17 @@
 
 // Input: [ 1.5, 3, 2.5, 1 ]
 // Output: 2
+
+
+const averageNum = arr => {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    const average = sum / arr.length;
+    return average;
+}
+
+console.log(averageNum([ 1 , 4 , 7 ]));
+console.log(averageNum([ 10, 5 ]));
+console.log(averageNum([ 1.5, 3, 2.5, 1 ]));
