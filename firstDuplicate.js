a809f24a8a2ed0cc323e0d5a68728287e456d84b
Gary Bergman <garybergman00@gmail.com>
2020-10-26 12:53:18 -0400

    Solved firstDuplicate

--- firstDuplicate.js
+++ firstDuplicate.js
@@ -9,3 +9,19 @@
 
 // Input: [ 2, 4, 5 ]
 // Output: undefined
+
+const firstDuplicate = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (arr[i] === arr[j]) {
+                return arr[i]
+            }
+        }
+    }
+}
+
+console.log(firstDuplicate([ 2, 5, 6, 3, 5 ]));
+
+console.log(firstDuplicate([ 1, 3, 4, 1, 3, 4 ]));
+
+console.log(firstDuplicate([ 2, 4, 5 ]));
