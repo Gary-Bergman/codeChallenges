da73d319cf7a8fd484d0d99f40037dfef9923985
Gary Bergman <garybergman00@gmail.com>
2020-10-28 12:43:08 -0400

    Solved dayOfWeek problem using switch case

--- dayOfWeek.js
+++ dayOfWeek.js
@@ -9,3 +9,43 @@
 
 // Input: 8
 // Output: undefined
+
+
+const dayOfWeek = num => {
+    switch (num) {
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 7:
+            day = "Sunday";
+            break;
+        default:
+            day = "Please enter a number between 1 and 7."
+    }
+
+    return day;
+}
+
+console.log(dayOfWeek(1));
+console.log(dayOfWeek(2));
+console.log(dayOfWeek(3));
+console.log(dayOfWeek(4));
+console.log(dayOfWeek(5));
+console.log(dayOfWeek(6));
+console.log(dayOfWeek(7));
+console.log(dayOfWeek(8));
