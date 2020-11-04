2144e94054bb21b4364df8032b6d3c7c877d1c61
Gary Bergman <garybergman00@gmail.com>
2020-11-04 14:06:25 -0500

    Solved numChange.js

--- numChange.js
+++ numChange.js
@@ -1,7 +1,7 @@
 // Write a function that takes an array of numbers and a function as parameters. The function paremeter should do something to a numbers (increment, double, decrement, etc) and return the result. Your function should return the array that results from applying the function parameter to each element in the number array. 
 
 // Ex:
-// Input: [1,2,3]  function(num){return num * 2*}
+// Input: [1,2,3]  function(num){return num * 2}
 // Output: [2,4,6]
 
 // Input: [1,2,3]  function(num){return num + 1}
@@ -12,3 +12,16 @@
 
 // Input: [1,2,3] function(num) {return num - 2}
 // Output: [-1, 0, 1]
+
+const numChange = (arr, func) => {
+    let resultsArray = [];
+    arr.forEach(num => {
+    resultsArray.push(func(num));
+    });
+    return resultsArray;
+}
+
+console.log(numChange([1, 2, 3], function (num) { return num * 2 }));
+console.log(numChange([1,2,3], function(num){return num + 1}));
+console.log(numChange([1,2,3], function(num) {return num /2}));
+console.log(numChange([1,2,3], function(num) {return num - 2}));
